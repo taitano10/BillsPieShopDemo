22714c75d7758fc0e6bfd9cd0a2dbe672c2e8184
taitano10 <51717074+taitano10@users.noreply.github.com>
2025-10-25 11:47:19 -0700

    Create the Db repository, query for data using LINQ

---
+++ BillsPieShopDemo/Models/CategoryRepository.cs
@@ -0,0 +1,13 @@
+﻿namespace BillsPieShopDemo.Models
+{
+    public class CategoryRepository: ICategoryRepository
+    {
+        private readonly BillsPieShopDbContext _billsPieShopDbContext;
+        public CategoryRepository(BillsPieShopDbContext billsPieShopDbContext)
+        {
+            _billsPieShopDbContext = billsPieShopDbContext;
+        }
+
+        public IEnumerable<Category> AllCategories => _billsPieShopDbContext.Categories.OrderBy(p => p.CategoryName);
+    }
+}
---
+++ BillsPieShopDemo/Models/PieRepository.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace BillsPieShopDemo.Models
+{
+    public class PieRepository : IPieRepository
+    {
+        private readonly BillsPieShopDbContext _billsPieShopDbContext;
+
+        public PieRepository(BillsPieShopDbContext billsPieShopDbContext)
+        {
+            _billsPieShopDbContext = billsPieShopDbContext;
+        }
+
+        public IEnumerable<Pie> AllPies
+        {
+            get
+            {
+                return _billsPieShopDbContext.Pies.Include(c => c.Category);
+            }
+        }
+
+        public IEnumerable<Pie> PiesOfTheWeek
+        {
+            get
+            {
+                return _billsPieShopDbContext.Pies.Include(c => c.Category).Where(p => p.IsPieOfTheWeek);
+            }
+        }
+
+        public Pie? GetPieById(int pieId)
+        {
+            return _billsPieShopDbContext.Pies.Include(c => c.Category).FirstOrDefault(p => p.PieId == pieId);
+        }
+    }
+}
