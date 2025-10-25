8ecf8c4ab936928f5c0643fa5597a5958072cccc
taitano10 <51717074+taitano10@users.noreply.github.com>
2025-10-24 18:06:53 -0700

    Add PieController for managing pie-related requests
    Introduces the `PieController` class in the
    `BillsPieShopDemo.Controllers` namespace. This controller
    handles HTTP requests for pies, utilizing `IPieRepository`
    and `ICategoryRepository` for data access. The `List` method
    retrieves all pies and returns them to the view for display.

---
+++ BillsPieShopDemo/Controllers/PieController.cs
@@ -0,0 +1,23 @@
+﻿using BillsPieShopDemo.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BillsPieShopDemo.Controllers
+{
+    public class PieController : Controller
+    {
+        private readonly IPieRepository _pieRepository;
+        private readonly ICategoryRepository _categoryRepository;
+
+        public PieController(IPieRepository pieRepository, ICategoryRepository categoryRepository)
+        {
+            _pieRepository = pieRepository;
+            _categoryRepository = categoryRepository;
+        }
+
+        public IActionResult List()
+        {
+            var pies = _pieRepository.AllPies;
+            return View(pies);
+        }
+    }
+}
