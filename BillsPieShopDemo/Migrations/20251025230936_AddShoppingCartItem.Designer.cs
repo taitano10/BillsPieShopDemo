4e1030d2036394833b01fd5c30be737ac1b89685
taitano10 <51717074+taitano10@users.noreply.github.com>
2025-10-25 16:13:30 -0700

    Shopping cart db migration
    --Generate the migration in package manager console
    
    >add-migration AddShoppingCartItem
    
    --To undo this action, use Remove-Migration.
    
    --bring database up in synch
    >update-database

--- BillsPieShopDemo/Migrations/20251025230936_AddShoppingCartItem.Designer.cs
+++ BillsPieShopDemo/Migrations/20251025230936_AddShoppingCartItem.Designer.cs
@@ -3,6 +3,7 @@ using BillsPieShopDemo.Models;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -10,9 +11,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace BillsPieShopDemo.Migrations
 {
     [DbContext(typeof(BillsPieShopDbContext))]
-    partial class BillsPieShopDbContextModelSnapshot : ModelSnapshot
+    [Migration("20251025230936_AddShoppingCartItem")]
+    partial class AddShoppingCartItem
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
