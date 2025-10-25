f689bb43d450b1705f358fb5ab6bc1437f91630b
taitano10 <51717074+taitano10@users.noreply.github.com>
2025-10-25 16:26:10 -0700

    Implement the shopping cart
    --create shopping cart class
    --create guid as new cart id
    --use session
    --check based on session if there is a value for cart id
    --use cookie to associate different requests from the same user from the same machine

---
+++ BillsPieShopDemo/Models/ShoppingCart.cs
@@ -0,0 +1,108 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace BillsPieShopDemo.Models
+{
+    public class ShoppingCart : IShoppingCart
+    {
+        private readonly BillsPieShopDbContext _billsPieShopDbContext;
+
+        public string? ShoppingCartId { get; set; }
+
+        public List<ShoppingCartItem> ShoppingCartItems { get; set; } = default!;
+
+        private ShoppingCart(BillsPieShopDbContext billsPieShopDbContext)
+        {
+           _billsPieShopDbContext = billsPieShopDbContext;
+        }
+
+        public static ShoppingCart GetCart(IServiceProvider services)
+        {
+            ISession? session = services.GetRequiredService<IHttpContextAccessor>()?.HttpContext?.Session;
+
+            BillsPieShopDbContext context = services.GetService<BillsPieShopDbContext>() ?? throw new Exception("Error initializing");
+
+            string cartId = session?.GetString("CartId") ?? Guid.NewGuid().ToString();
+
+            session?.SetString("CartId", cartId);
+
+            return new ShoppingCart(context) { ShoppingCartId = cartId };
+        }
+
+        public void AddToCart(Pie pie)
+        {
+            var shoppingCartItem =
+                   _billsPieShopDbContext.ShoppingCartItems.SingleOrDefault(
+                        s => s.Pie.PieId == pie.PieId && s.ShoppingCartId == ShoppingCartId);
+
+            if (shoppingCartItem == null)
+            {
+                shoppingCartItem = new ShoppingCartItem
+                {
+                    ShoppingCartId = ShoppingCartId,
+                    Pie = pie,
+                    Amount = 1
+                };
+
+               _billsPieShopDbContext.ShoppingCartItems.Add(shoppingCartItem);
+            }
+            else
+            {
+                shoppingCartItem.Amount++;
+            }
+           _billsPieShopDbContext.SaveChanges();
+        }
+
+        public int RemoveFromCart(Pie pie)
+        {
+            var shoppingCartItem =
+                   _billsPieShopDbContext.ShoppingCartItems.SingleOrDefault(
+                        s => s.Pie.PieId == pie.PieId && s.ShoppingCartId == ShoppingCartId);
+
+            var localAmount = 0;
+
+            if (shoppingCartItem != null)
+            {
+                if (shoppingCartItem.Amount > 1)
+                {
+                    shoppingCartItem.Amount--;
+                    localAmount = shoppingCartItem.Amount;
+                }
+                else
+                {
+                   _billsPieShopDbContext.ShoppingCartItems.Remove(shoppingCartItem);
+                }
+            }
+
+           _billsPieShopDbContext.SaveChanges();
+
+            return localAmount;
+        }
+
+        public List<ShoppingCartItem> GetShoppingCartItems()
+        {
+            return ShoppingCartItems ??=
+                      _billsPieShopDbContext.ShoppingCartItems.Where(c => c.ShoppingCartId == ShoppingCartId)
+                           .Include(s => s.Pie)
+                           .ToList();
+        }
+
+        public void ClearCart()
+        {
+            var cartItems =_billsPieShopDbContext
+                .ShoppingCartItems
+                .Where(cart => cart.ShoppingCartId == ShoppingCartId);
+
+           _billsPieShopDbContext.ShoppingCartItems.RemoveRange(cartItems);
+
+           _billsPieShopDbContext.SaveChanges();
+        }
+
+        public decimal GetShoppingCartTotal()
+        {
+            var total =_billsPieShopDbContext.ShoppingCartItems.Where(c => c.ShoppingCartId == ShoppingCartId)
+                .Select(c => c.Pie.Price * c.Amount).Sum();
+            return total;
+        }
+    }
+
+}
