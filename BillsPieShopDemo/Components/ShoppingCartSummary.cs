26f02481b4d33f9dfd89d72dfe6c18543ac3b1fb
taitano10 <51717074+taitano10@users.noreply.github.com>
2025-10-25 19:03:02 -0700

    Add a view component to show items in basket
    --create a navigation view component
    --create a shopping cart view component

---
+++ BillsPieShopDemo/Components/ShoppingCartSummary.cs
@@ -0,0 +1,27 @@
+﻿using BillsPieShopDemo.Models;
+using BillsPieShopDemo.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BillsPieShopDemo.Components
+{
+    public class ShoppingCartSummary: ViewComponent
+    {
+        private readonly IShoppingCart _shoppingCart;
+        public ShoppingCartSummary(IShoppingCart shoppingCart)
+        {
+            _shoppingCart = shoppingCart;
+        }
+
+        public IViewComponentResult Invoke()
+        {
+            //var items = new List<ShoppingCartItem>() { new ShoppingCartItem(), new ShoppingCartItem() };
+
+            var items = _shoppingCart.GetShoppingCartItems();
+            _shoppingCart.ShoppingCartItems = items;
+
+            var shoppingCartViewModel = new ShoppingCartViewModel(_shoppingCart, _shoppingCart.GetShoppingCartTotal());
+
+            return View(shoppingCartViewModel);
+        }
+    }
+}
--- BillsPieShopDemo/Controllers/PieController.cs
+++ BillsPieShopDemo/Controllers/PieController.cs
@@ -15,11 +15,31 @@ namespace BillsPieShopDemo.Controllers
             _categoryRepository = categoryRepository;
         }
 
-        public IActionResult List()
+        //public IActionResult List()
+        //{
+        //    PieListViewModel piesListViewModel = new PieListViewModel
+        //        (_pieRepository.AllPies, "All Pies");
+        //    return View(piesListViewModel);
+        //}
+
+        public ViewResult List(string category)
         {
-            PieListViewModel piesListViewModel = new PieListViewModel
-                (_pieRepository.AllPies, "All Pies");
-            return View(piesListViewModel);
+            IEnumerable<Pie> pies;
+            string? currentCategory;
+
+            if (string.IsNullOrEmpty(category))
+            {
+                pies = _pieRepository.AllPies.OrderBy(p => p.PieId);
+                currentCategory = "All pies";
+            }
+            else
+            {
+                pies = _pieRepository.AllPies.Where(p => p.Category.CategoryName == category)
+                    .OrderBy(p => p.PieId);
+                currentCategory = _categoryRepository.AllCategories.FirstOrDefault(c => c.CategoryName == category)?.CategoryName;
+            }
+
+            return View(new PieListViewModel(pies, currentCategory));
         }
 
         public IActionResult Details(int id)
