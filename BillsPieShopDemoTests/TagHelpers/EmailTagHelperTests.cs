288aeea4ef58a7ef7b930f98fcaa468480ee047c
taitano10 <51717074+taitano10@users.noreply.github.com>
2025-10-25 22:53:18 -0700

    Write Unit Tests
    --add xUnit unit testing project
    --test the controllers
    --test a tag helper

---
+++ BillsPieShopDemoTests/TagHelpers/EmailTagHelperTests.cs
@@ -0,0 +1,38 @@
+﻿using BillsPieShopDemo.TagHelpers;
+using Microsoft.AspNetCore.Razor.TagHelpers;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BillsPieShopDemoTests.TagHelpers
+{
+    public class EmailTagHelperTests
+    {
+        [Fact]
+        public void Generates_Email_Link()
+        {
+            EmailTagHelper emailTagHelper = new EmailTagHelper() { Address = "test@billspieshop.com", Content = "Email" }; ;
+
+            var tagHelperContext = new TagHelperContext(
+                new TagHelperAttributeList(),
+                new Dictionary<object, object>(), string.Empty);
+
+            var content = new Mock<TagHelperContent>();
+
+            var tagHelperOutput = new TagHelperOutput("a",
+                new TagHelperAttributeList(),
+                (cache, encoder) => Task.FromResult(content.Object));
+
+            // Act
+            emailTagHelper.Process(tagHelperContext, tagHelperOutput);
+
+            Assert.Equal("Email", tagHelperOutput.Content.GetContent());
+            Assert.Equal("a", tagHelperOutput.TagName);
+            Assert.Equal("mailto:test@billspieshop.com", tagHelperOutput.Attributes[0].Value);
+        }
+
+    }
+}
